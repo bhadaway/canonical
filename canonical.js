ad9be0eeebc7001b55e26aa7c489db8e57d285fb
Bryan Hadaway <bhadaway@gmail.com>
2025-03-26 05:48:07 -0600

    1.3

--- canonical.js
+++ canonical.js
@@ -1,12 +1,16 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-	const canonical = document.querySelector('link[rel="canonical"]');
-	if (!canonical || !canonical.href) {
-		return
+	canon();
+	setTimeout(canon, 5000);
+	function canon() {
+		const canonical = document.querySelector('link[rel="canonical"]');
+		if (!canonical || !canonical.href) {
+			return
+		}
+		chrome.runtime.sendMessage({
+			method: `${location.href === canonical.href ? 'is' : 'offer'}-canonical`,
+			url: canonical.href
+		});
 	}
-	chrome.runtime.sendMessage({
-		method: `${location.href === canonical.href ? 'is' : 'offer'}-canonical`,
-		url: canonical.href
-	});
 });
